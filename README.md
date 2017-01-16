08a28bc8288b680a506d2a1422eaf16730c803d0
Andrea Giammarchi <andrea.giammarchi@mailonline.co.uk>
2017-01-16 17:11:38 +0000

    testing the code coverage tool

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# backtick-template
-ES2015 backticks for ES3+ engines
+# ES2015 backticks for ES3+ engines
+
+1:1 native ``backticks`${template}``` 100% code coverage.
+
+```js
+var template = require('backtick-template');
+
+// just string
+const info = 'template';
+`some ${info}` === template('some ${info}', {info});
+
+// passing through a transformer
+transform `some ${info}` ===
+    template(transform, 'some ${info}', {info});
+
+// using it as String method
+String.prototype.template = template.asMethod;
+
+`some ${info}` === 'some ${info}'.template({info});
+
+transform `some ${info}` ===
+    'some ${info}'.template(transform, {info});
+
+```
---
+++ test.js
@@ -0,0 +1,107 @@
+const console = require('consolemd');
+const template = require('./backtick-template');
+
+const test = (chunks, ...rest) =>
+    chunks.reduce((prev, curr, i) => prev + rest[i - 1] + curr);
+
+console.log('# Backtick Template Test');
+console.time('test');
+
+
+console.log('"" empty');
+console.assert(test`` === template(''));
+console.assert(test`` === template('', {}));
+console.assert(test`` === template(test, ''));
+console.assert(test`` === template(test, '', {}));
+
+
+console.log('${1}');
+console.assert(test`${1}` === template('${1}'));
+console.assert(test`${1}` === template('${1}', {}));
+console.assert(test`${1}` === template(test, '${1}'));
+console.assert(test`${1}` === template(test, '${1}', {}));
+
+
+console.log('before${1}');
+console.assert(test`before${1}` === template('before${1}'));
+console.assert(test`before${1}` === template('before${1}', {}));
+console.assert(test`before${1}` === template(test, 'before${1}'));
+console.assert(test`before${1}` === template(test, 'before${1}', {}));
+
+
+console.log('${1}after');
+console.assert(test`${1}after` === template('${1}after'));
+console.assert(test`${1}after` === template('${1}after', {}));
+console.assert(test`${1}after` === template(test, '${1}after'));
+console.assert(test`${1}after` === template(test, '${1}after', {}));
+
+
+console.log('ar${1}ound');
+console.assert(test`ar${1}ound` === template('ar${1}ound'));
+console.assert(test`ar${1}ound` === template('ar${1}ound', {}));
+console.assert(test`ar${1}ound` === template(test, 'ar${1}ound'));
+console.assert(test`ar${1}ound` === template(test, 'ar${1}ound', {}));
+
+
+console.log('ar${one}ou${two}nd');
+const one = 1, two = 2;
+console.assert(test`ar${one}ou${two}nd` === template('ar${one}ou${two}nd', {one, two}));
+console.assert(test`ar${one}ou${two}nd` === template(test, 'ar${one}ou${two}nd', {one, two}));
+
+
+console.log('hm ... ${f({a: v})}');
+const f = function (_) { return 'foo'; }, v = 1;
+console.assert(test`hm ... ${f({a: v})}` === template('hm ... ${f({a: v})}', {f, v}));
+console.assert(test`hm ... ${f({a: v})}` === template(test, 'hm ... ${f({a: v})}', {f, v}));
+
+
+String.prototype.template = template.asMethod;
+console.log('"".template()');
+console.assert(test`` === ''.template());
+console.assert(test`` === ''.template({}));
+console.assert(test`` === ''.template(test));
+console.assert(test`` === ''.template(test, {}));
+
+
+console.log('"${1}".template()');
+console.assert(test`${1}` === '${1}'.template());
+console.assert(test`${1}` === '${1}'.template({}));
+console.assert(test`${1}` === '${1}'.template(test));
+console.assert(test`${1}` === '${1}'.template(test, {}));
+
+
+console.log('"before${1}".template()');
+console.assert(test`before${1}` === 'before${1}'.template());
+console.assert(test`before${1}` === 'before${1}'.template({}));
+console.assert(test`before${1}` === 'before${1}'.template(test));
+console.assert(test`before${1}` === 'before${1}'.template(test, {}));
+
+
+console.log('"${1}after".template()');
+console.assert(test`${1}after` === '${1}after'.template());
+console.assert(test`${1}after` === '${1}after'.template({}));
+console.assert(test`${1}after` === '${1}after'.template(test));
+console.assert(test`${1}after` === '${1}after'.template(test, {}));
+
+
+console.log('"ar${1}ound".template()');
+console.assert(test`ar${1}ound` === 'ar${1}ound'.template());
+console.assert(test`ar${1}ound` === 'ar${1}ound'.template('ar${1}ound', {}));
+console.assert(test`ar${1}ound` === 'ar${1}ound'.template(test));
+console.assert(test`ar${1}ound` === 'ar${1}ound'.template(test, {}));
+
+
+console.log('"ar${one}ou${two}nd".template({one, two})');
+console.assert(test`ar${one}ou${two}nd` === 'ar${one}ou${two}nd'.template({one, two}));
+console.assert(test`ar${one}ou${two}nd` === 'ar${one}ou${two}nd'.template(test, {one, two}));
+
+
+console.log('"hm ... ${f({a: v})}".template()');
+console.assert(test`hm ... ${f({a: v})}` === 'hm ... ${f({a: v})}'.template({f, v}));
+console.assert(test`hm ... ${f({a: v})}` === 'hm ... ${f({a: v})}'.template(test, {f, v}));
+
+
+console.log('');
+console.timeEnd('test');
+console.log('#green(*✔* OK)');
+console.log('');
